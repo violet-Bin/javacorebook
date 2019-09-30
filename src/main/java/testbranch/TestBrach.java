8103065c626581771d6715fec43cfd48dd5f2d31
jiangjiabin3 <jiangjiabin5@jd.com>
2019-09-30 15:06:27 +0800

    create branch

---
+++ src/main/java/testbranch/TestBrach.java
@@ -0,0 +1,14 @@
+package testbranch;
+
+/**
+ * @Author: jiangjiabin3
+ * @Date: Created in 2019/9/30 15:04
+ * @Description:
+ */
+public class TestBrach {
+
+    public static void main(String[] args) {
+        System.out.println("啊啊啊啊啊啊啊啊");
+    }
+
+}
